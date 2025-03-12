4ac48496e429135a0f5212fbd2dc68fdfac859bf
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-03-12 18:47:30 +0100

    remove(repository): @DataJpaTest

--- src/test/java/infrastructure/persistence/PersistedEventRepositoryTest.java
+++ src/test/java/infrastructure/persistence/PersistedEventRepositoryTest.java
@@ -2,17 +2,17 @@ package infrastructure.persistence;
 
 import domain.spi.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@DataJpaTest
+@SpringBootTest
 class PersistedEventRepositoryTest implements EventRepositoryTest {
 
     @Autowired
-    private JpaEventRepository jpaRepository;
+    private PersistedEventRepository eventRepository;
 
     @Override
     public EventRepository provideRepositoryImplementation() {
-        return new PersistedEventRepository(jpaRepository);
+        return eventRepository;
     }
 
 }
