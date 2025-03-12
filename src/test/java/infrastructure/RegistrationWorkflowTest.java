3e8fd490f3db79a3db7d75d45871374ea2f02d66
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-03-12 17:47:50 +0100

    remove(workflow): @Order annotations

--- src/test/java/infrastructure/RegistrationWorkflowTest.java
+++ src/test/java/infrastructure/RegistrationWorkflowTest.java
@@ -3,7 +3,6 @@ package infrastructure;
 import infrastructure.rest.dto.AttendeeRequestBody;
 import infrastructure.rest.dto.EventRequestBody;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -36,27 +35,28 @@ class RegistrationWorkflowTest {
     private final LocalDate tomorrow = LocalDate.now().plusDays(1);
 
     @Test
-    @Order(1)
+    void scenario() throws Exception {
+        there_is_no_events();
+        an_organizer_plans_an_event();
+        there_is_an_event_available();
+        someone_registers_to_the_event();
+        the_event_is_full();
+    }
+
     void there_is_no_events() throws Exception {
         api.consultEventCalendar()
                 .andExpect(jsonPath("$.events").isEmpty());
     }
 
-    @Test
-    @Order(2)
     void an_organizer_plans_an_event() throws Exception {
         UUID organizerId = testDataFactory.givenAnOrganizer("ee8b1e44-b0e1-4cc0-9d6e-b24ce343111e");
 
         EventRequestBody event = new EventRequestBody("An event", tomorrow, 1);
 
         eventId = api.planAnEvent(event, organizerId)
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
+                .andReturn().getResponse().getContentAsString();
     }
 
-    @Test
-    @Order(3)
     void there_is_an_event_available() throws Exception {
         api.consultEventCalendar()
                 .andExpect(content().json("""
@@ -72,8 +72,6 @@ class RegistrationWorkflowTest {
                         """.formatted(tomorrow)));
     }
 
-    @Test
-    @Order(4)
     void someone_registers_to_the_event() throws Exception {
         api.registerToEvent(eventId, new AttendeeRequestBody("Amy", "amy@email.com"));
 
@@ -81,8 +79,6 @@ class RegistrationWorkflowTest {
                 .andExpect(jsonPath("$.events[0].attendees").value(1));
     }
 
-    @Test
-    @Order(5)
     void the_event_is_full() throws Exception {
         api.registerToEvent(eventId, new AttendeeRequestBody("Rory", "rory@email.com"))
                 .andExpect(status().isBadRequest());
