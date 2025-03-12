fa38cbe83e489f6a14847ab076e7d67741a24db9
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-03-12 18:48:58 +0100

    remove(repository): test interface

--- src/test/java/infrastructure/persistence/PersistedEventRepositoryTest.java
+++ src/test/java/infrastructure/persistence/PersistedEventRepositoryTest.java
@@ -1,18 +1,28 @@
 package infrastructure.persistence;
 
-import domain.spi.EventRepository;
+import domain.model.Event;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import testing.extensions.EventResolver;
+
+import static testing.assertions.Assertions.assertThat;
 
 @SpringBootTest
-class PersistedEventRepositoryTest implements EventRepositoryTest {
+@ExtendWith(EventResolver.class)
+class PersistedEventRepositoryTest {
 
     @Autowired
     private PersistedEventRepository eventRepository;
 
-    @Override
-    public EventRepository provideRepositoryImplementation() {
-        return eventRepository;
+    @Test
+    void should_save_then_find_event(Event event) {
+        eventRepository.save(event);
+
+        assertThat(eventRepository.findAll())
+                .hasSize(1)
+                .containsExactly(event);
     }
 
 }
