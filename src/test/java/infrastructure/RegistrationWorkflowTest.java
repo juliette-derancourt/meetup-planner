12cbd98891680cb283661a6b37f78ac0bb4b05f3
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-03-12 17:51:47 +0100

    remove(workflow): @Order annotations

--- src/test/java/infrastructure/RegistrationWorkflowTest.java
+++ src/test/java/infrastructure/RegistrationWorkflowTest.java
@@ -2,11 +2,7 @@ package infrastructure;
 
 import infrastructure.rest.dto.AttendeeRequestBody;
 import infrastructure.rest.dto.EventRequestBody;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,37 +21,37 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith({UserInterfaceExtension.class, TestDataFactoryExtension.class})
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RegistrationWorkflowTest {
 
     UserInterface userInterface;
+    TestDataFactory testDataFactory;
 
     private String eventId;
     private final LocalDate tomorrow = LocalDate.now().plusDays(1);
 
     @Test
-    @Order(1)
+    void scenario() throws Exception {
+        there_is_no_events();
+        an_organizer_plans_an_event();
+        there_is_an_event_available();
+        someone_registers_to_the_event();
+        the_event_is_full();
+    }
+
     void there_is_no_events() throws Exception {
         userInterface.consultEventCalendar()
                 .andExpect(jsonPath("$.events").isEmpty());
     }
 
-    @Test
-    @Order(2)
-    void an_organizer_plans_an_event(TestDataFactory testDataFactory) throws Exception {
+    void an_organizer_plans_an_event() throws Exception {
         UUID organizerId = testDataFactory.givenAnOrganizer("ee8b1e44-b0e1-4cc0-9d6e-b24ce343111e");
 
         EventRequestBody event = new EventRequestBody("An event", tomorrow, 1);
 
         eventId = userInterface.planAnEvent(event, organizerId)
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
+                .andReturn().getResponse().getContentAsString();
     }
 
-    @Test
-    @Order(3)
     void there_is_an_event_available() throws Exception {
         userInterface.consultEventCalendar()
                 .andExpect(content().json("""
@@ -71,8 +67,6 @@ class RegistrationWorkflowTest {
                         """.formatted(tomorrow)));
     }
 
-    @Test
-    @Order(4)
     void someone_registers_to_the_event() throws Exception {
         userInterface.registerToEvent(eventId, new AttendeeRequestBody("Amy", "amy@email.com"));
 
@@ -80,8 +74,6 @@ class RegistrationWorkflowTest {
                 .andExpect(jsonPath("$.events[0].attendees").value(1));
     }
 
-    @Test
-    @Order(5)
     void the_event_is_full() throws Exception {
         userInterface.registerToEvent(eventId, new AttendeeRequestBody("Rory", "rory@email.com"))
                 .andExpect(status().isBadRequest());
