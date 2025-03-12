af09c6fa30458fa9152c5db58460112d6b75a5a9
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-03-12 17:47:50 +0100

    remove(workflow): @Order annotations

--- src/test/java/infrastructure/RegistrationWorkflowTest.java
+++ src/test/java/infrastructure/RegistrationWorkflowTest.java
@@ -2,11 +2,15 @@ package infrastructure;
 
 import infrastructure.rest.dto.AttendeeRequestBody;
 import infrastructure.rest.dto.EventRequestBody;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import testing.WorkflowTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import testing.dsl.RestApi;
 import testing.dsl.TestDataFactory;
+import testing.dsl.RestApi;
+import testing.extensions.TestDataFactoryExtension;
+import testing.extensions.RestApiExtension;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -15,36 +19,40 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WorkflowTest
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith({RestApiExtension.class, TestDataFactoryExtension.class})
 class RegistrationWorkflowTest {
 
     RestApi api;
+    TestDataFactory testDataFactory;
 
     private String eventId;
     private final LocalDate tomorrow = LocalDate.now().plusDays(1);
 
     @Test
-    @Order(1)
+    void scenario() throws Exception {
+        there_is_no_events();
+        an_organizer_plans_an_event();
+        there_is_an_event_available();
+        someone_registers_to_the_event();
+        the_event_is_full();
+    }
+
     void there_is_no_events() throws Exception {
         api.consultEventCalendar()
                 .andExpect(jsonPath("$.events").isEmpty());
     }
 
-    @Test
-    @Order(2)
-    void an_organizer_plans_an_event(TestDataFactory testDataFactory) throws Exception {
+    void an_organizer_plans_an_event() throws Exception {
         UUID organizerId = testDataFactory.givenAnOrganizer("ee8b1e44-b0e1-4cc0-9d6e-b24ce343111e");
 
         EventRequestBody event = new EventRequestBody("An event", tomorrow, 1);
 
         eventId = api.planAnEvent(event, organizerId)
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
+                .andReturn().getResponse().getContentAsString();
     }
 
-    @Test
-    @Order(3)
     void there_is_an_event_available() throws Exception {
         api.consultEventCalendar()
                 .andExpect(content().json("""
@@ -60,8 +68,6 @@ class RegistrationWorkflowTest {
                         """.formatted(tomorrow)));
     }
 
-    @Test
-    @Order(4)
     void someone_registers_to_the_event() throws Exception {
         api.registerToEvent(eventId, new AttendeeRequestBody("Amy", "amy@email.com"));
 
@@ -69,8 +75,6 @@ class RegistrationWorkflowTest {
                 .andExpect(jsonPath("$.events[0].attendees").value(1));
     }
 
-    @Test
-    @Order(5)
     void the_event_is_full() throws Exception {
         api.registerToEvent(eventId, new AttendeeRequestBody("Rory", "rory@email.com"))
                 .andExpect(status().isBadRequest());
