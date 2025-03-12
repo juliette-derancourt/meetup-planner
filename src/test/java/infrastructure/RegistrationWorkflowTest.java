ecae483a57f2abd4b17df07e8d1bf8f0bce93eed
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-03-12 17:40:00 +0100

    rename workflow test

--- src/test/java/infrastructure/RegistrationWorkflowTest.java
+++ src/test/java/infrastructure/RegistrationWorkflowTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WorkflowTest
-class EventWorkflowTest {
+class RegistrationWorkflowTest {
 
     private UserInterface userInterface;
 
