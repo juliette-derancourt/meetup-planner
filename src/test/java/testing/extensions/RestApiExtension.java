d9f118179072cfcfbf2e92601e346875fd900600
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-05-30 00:25:35 +0200

    improve RestApiExtension

--- src/test/java/testing/extensions/RestApiExtension.java
+++ src/test/java/testing/extensions/RestApiExtension.java
@@ -7,17 +7,27 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.ReflectionUtils;
 import testing.dsl.RestApi;
+
+import java.lang.reflect.Field;
 
 public class RestApiExtension implements TestInstancePostProcessor {
 
     @Override
     public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
-        RestApi restApi = buildRestApi(context);
-        setFieldInTestClass(testInstance, restApi);
+        if (hasRestApiField(testInstance)) {
+            RestApi restApi = createRestApi(context);
+            setFieldInTestInstance(testInstance, restApi);
+        }
     }
 
-    private static RestApi buildRestApi(ExtensionContext context) {
+    private boolean hasRestApiField(Object testInstance) {
+        Field field = ReflectionUtils.findField(testInstance.getClass(), null, RestApi.class);
+        return field != null;
+    }
+
+    private static RestApi createRestApi(ExtensionContext context) {
         ApplicationContext applicationContext = SpringExtension.getApplicationContext(context);
         MockMvc mockMvc = applicationContext.getBean(MockMvc.class);
         ObjectMapper objectMapper = applicationContext.getBean(ObjectMapper.class);
@@ -25,12 +35,8 @@ public class RestApiExtension implements TestInstancePostProcessor {
         return new RestApi(mockMvc, objectMapper);
     }
 
-    private static void setFieldInTestClass(Object testInstance, RestApi restApi) {
-        try {
-            ReflectionTestUtils.setField(testInstance, null, restApi, RestApi.class);
-        } catch (Exception e) {
-            // no field to set
-        }
+    private static void setFieldInTestInstance(Object testInstance, RestApi restApi) {
+        ReflectionTestUtils.setField(testInstance, null, restApi, RestApi.class);
     }
 
 }
