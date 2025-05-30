6d245167f923acdab6d633799a87dce85e1a9677
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-05-30 10:52:18 +0200

    remove: parallel execution configuration

--- src/test/java/infrastructure/rest/EventControllerTest.java
+++ src/test/java/infrastructure/rest/EventControllerTest.java
@@ -13,8 +13,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,7 +41,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith({EventResolver.class})
-@Execution(ExecutionMode.SAME_THREAD)
 class EventControllerTest {
 
     @MockitoBean
