a5ef4ed4c1b4e0e5f17e8e403209e9a7090633ac
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-06-11 18:15:22 +0200

    remove(unit): parameterized tests / resolvers

--- src/test/java/domain/EventPlannerTest.java
+++ src/test/java/domain/EventPlannerTest.java
@@ -3,20 +3,21 @@ package domain;
 import domain.api.EventPlanner;
 import domain.fakes.FakeClock;
 import domain.model.Event;
-import domain.model.exceptions.UnauthorizedActionException;
 import infrastructure.auth.SimpleAuthorizationProvider;
 import infrastructure.persistence.InMemoryEventRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import testing.extensions.Random;
+import testing.extensions.UuidResolver;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static testing.assertions.Assertions.assertThat;
 import static testing.assertions.EventAssert.EVENT;
 
+@ExtendWith(UuidResolver.class)
 class EventPlannerTest {
 
     private final InMemoryEventRepository eventRepository = new InMemoryEventRepository();
@@ -37,17 +38,6 @@ class EventPlannerTest {
                 .first(EVENT)
                 .hasName("An event")
                 .isHeldAtDate(date);
-    }
-
-    @Test
-    void only_organizers_are_able_to_plan_an_event() {
-        LocalDate date = LocalDate.of(2025, 6, 12);
-
-        assertThatExceptionOfType(UnauthorizedActionException.class)
-                .isThrownBy(() -> eventPlanner.planEvent("An event", date, 50, UUID.randomUUID()))
-                .withMessage("User is not authorized to perform action: plan an event");
-
-        assertThat(eventRepository.findAll()).isEmpty();
     }
 
 }
