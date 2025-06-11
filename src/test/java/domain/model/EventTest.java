e179cd8f6bb912fc5823b635fdc966a682a5b835
Juliette de Rancourt <derancourt.juliette@gmail.com>
2025-06-12 00:24:45 +0200

    remove(unit): attendee resolver

--- src/test/java/domain/model/EventTest.java
+++ src/test/java/domain/model/EventTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import testing.extensions.AttendeeResolver;
-import testing.extensions.DateResolver;
 import testing.extensions.EventResolver;
 
 import java.time.LocalDate;
@@ -18,13 +16,14 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import static testing.assertions.Assertions.assertThat;
 import static testing.extensions.EventResolver.Full;
 
-@ExtendWith({EventResolver.class, AttendeeResolver.class, DateResolver.class})
+@ExtendWith(EventResolver.class)
 class EventTest {
 
     private final FakeClock clock = new FakeClock();
 
     @Test
-    void should_create_an_event(LocalDate date) {
+    void should_create_an_event() {
+        LocalDate date = LocalDate.of(2025, 2, 1);
         Event event = Event.create("Event name", date, 10);
 
         assertThat(event).hasAnId()
@@ -35,28 +34,36 @@ class EventTest {
 
     @ParameterizedTest
     @ValueSource(ints = {-10, 0})
-    void should_not_create_an_event_with_no_capacity(int venueCapacity, LocalDate date) {
+    void should_not_create_an_event_with_no_capacity(int venueCapacity) {
+        LocalDate date = LocalDate.of(2025, 2, 1);
+
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> Event.create("Event name", date, venueCapacity))
                 .withMessage("Venue capacity must be greater than zero");
     }
 
     @Test
-    void should_register_an_attendee(Event event, Attendee attendee) {
+    void should_register_an_attendee(Event event) {
+        Attendee attendee = Attendee.withPersonalInformation("An attendee", "test@email.com");
+
         event.register(attendee, clock);
 
         assertThat(event).isAttendedBy(attendee);
     }
 
     @Test
-    void should_not_register_someone_when_the_event_is_full(@Full Event event, Attendee attendee) {
+    void should_not_register_someone_when_the_event_is_full(@Full Event event) {
+        Attendee attendee = Attendee.withPersonalInformation("An attendee", "test@email.com");
+
         assertThatExceptionOfType(EventAlreadyFullException.class)
                 .isThrownBy(() -> event.register(attendee, clock));
     }
 
     @Test
-    void should_not_register_when_the_event_is_already_over(Event event, Attendee attendee) {
+    void should_not_register_when_the_event_is_already_over(Event event) {
         clock.setDate(event.date().plusDays(1));
+
+        Attendee attendee = Attendee.withPersonalInformation("An attendee", "test@email.com");
 
         assertThatExceptionOfType(EventIsOverException.class)
                 .isThrownBy(() -> event.register(attendee, clock));
